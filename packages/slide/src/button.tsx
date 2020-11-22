7ed42415f4fdc59f839dc1490cbbba68fb2c87fb
Inácio Schweller <iscmendonca@gmail.com>
2020-11-22 13:05:31 +0100

    feat: first prototype of slide target button

--- packages/slide/src/button.tsx
+++ packages/slide/src/button.tsx
@@ -1,9 +1,63 @@
 import * as React from "react";
+import {
+  motion,
+  MotionValue,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 import { forwardRef } from "./base";
 
-export interface Props {}
+export interface Props {
+  children?: React.ReactNode;
+}
 
-export const Button = forwardRef<Props, "button">(function Button(props, ref) {
+const DragCircleIcon = ({ x }: { x: MotionValue }) => (
+  <svg
+    viewBox="0 0 5 8"
+    style={{
+      fill: "#fff",
+      stroke: "none",
+      width: "10px",
+      height: "16px",
+      ...x,
+    }}
+  >
+    <motion.polygon points="1.02941176 8 0 6.96296296 2.94117647 4 0 1.03703704 1.02941176 0 5 4" />
+  </svg>
+);
+
+export const Button = forwardRef<Props, "button">(function Button(
+  props: Props
+) {
   const { children } = props;
-  return <button>{children}</button>;
+  const x = useMotionValue(0);
+  const background = useTransform(
+    x,
+    [-100, 0, 100],
+    ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
+  );
+
+  return (
+    <motion.div
+      style={{
+        background,
+        display: "table",
+        width: "136px",
+        height: "60px",
+        borderRadius: "6px",
+        cursor: "-webkit-grab",
+        padding: "6px 8px",
+      }}
+    >
+      <motion.div
+        drag="x"
+        dragConstraints={{ left: 0, right: 0 }}
+        style={{
+          x,
+        }}
+      >
+        <DragCircleIcon x={x} />
+      </motion.div>
+    </motion.div>
+  );
 });
--- packages/slide/tests/index.test.ts
+++ packages/slide/tests/index.test.ts
@@ -1,5 +1,1 @@
-import { add } from "../src";
-
-test("should add", () => {
-  expect(add(1, 1)).toEqual(2);
-});
+test.todo("render button");
